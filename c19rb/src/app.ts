a26efea9dbb7baa27d62f908ca9ad3da57363e7c
mbellezo <mbellezo@gmail.com>
2020-08-15 11:19:07 -0300

    Configuration of logging with morgan

--- c19rb/src/app.ts
+++ c19rb/src/app.ts
@@ -1,8 +1,9 @@
 import express from 'express'
+import morgan from 'morgan'
 import { router } from './routes'
 
 const app = express()
-
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(router)
 
--- c19rb/src/server.ts
+++ c19rb/src/server.ts
@@ -1,3 +1,6 @@
 import { app } from './app'
 
-app.listen(3333)
+const PORT = process.env.PORT || 3333
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`)
+})
